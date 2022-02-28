c9145278899ba91805694b98e5e0a7c9b716180e
mhmdalsharif1 <m.sharif@prodigy-sys.com>
2022-02-28 19:00:18 +0300

    update test

--- script.js
+++ script.js
@@ -1,6 +1,26 @@
+
 function compute()
+
 {
-    p = document.getElementById("principal").value;
-    
+
+var principal = document.getElementById("principal").value;
+if(principal <1)
+{alert("Enter a positive number");
+
+}
+else{
+var rate = document.getElementById("rate").value;
+var years = document.getElementById("years").value;
+
+var year = new Date().getFullYear()+parseInt(years);
+var interest = principal * years * rate /100;
+ 
+      document.getElementById("result").innerText=interest;}
+}
+
+function updateRate() 
+{
+    var rateval = document.getElementById("rate").value;
+    document.getElementById("rate_val").innerText=rateval;
 }
         
